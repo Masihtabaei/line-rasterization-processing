28cb04139e91f59d30b331c55304f045e9fc649a
Seyedmasih Tabaei <seyedmasih.tabaei@stud.hs-coburg.de>
2024-05-19 00:32:59 +0200

    Implemented the naiive algorithm for line rasterization in the first octant and with a slope between smaller or equal to 1

--- LineRasterizer.java
+++ LineRasterizer.java
@@ -1,22 +1,28 @@
 public final class LineRasterizer
 {
-  private static final void drawLine(int[] framebuffer, int w, int2 a, int2 b, int col)
-  {
-    int y = 0;
-    int err = 0;
-    for (int x = a.x; x <= b.x; x++)
-    {
-      if (err >= b.x)
-      {
-        y++;
-        err += -2 * b.x;
-      }
-      err += 2 * b.y;
-      framebuffer[y * w + x] = col;
-    }
-  }
+  //private static final void drawLine(int[] framebuffer, int w, int2 a, int2 b, int col)
+  //{
+  //  int y = 0;
+  //  int err = 0;
+  //  for (int x = a.x; x <= b.x; x++)
+  //  {
+  //    if (err >= b.x)
+  //    {
+  //      y++;
+  //      err += -2 * b.x;
+  //    }
+  //    err += 2 * b.y;
+  //    framebuffer[y * w + x] = col;
+  //  }
+  //}
   
   private static final void drawLine(int[] frameBuffer, int widthOfDisplayWindow, int2 destinationPointAsVector, int pixelColor){
+    float slope = destinationPointAsVector.y / (float) destinationPointAsVector.x;
+    System.out.println("Slope: " + slope);
+    for(int x = 0; x <= destinationPointAsVector.x; x++){
+      int y = Math.round(slope * x);
+      frameBuffer[y * widthOfDisplayWindow + x] = pixelColor;
+    }
     
   }
 
